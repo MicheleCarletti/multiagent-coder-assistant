ad70dfb93bfbdb4b6ccd79297ac2bbbf8d5d4b1e
Michele Carletti <michele.carletti@sidel.com>
2026-02-13 15:32:26 +0100

    streamlit config and run.sh update

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ set -e
 python --version
 pip freeze
 
-# Run the main application
-exec python -m streamlit run main_ui.py --server.port 8000
+# Run the main application for Azure App Service
+exec python -m streamlit run main_ui.py --server.port 8000 --server.address 0.0.0.0
